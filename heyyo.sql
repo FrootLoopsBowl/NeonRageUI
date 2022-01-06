3d099c9bb56e27abf226e8fc807ba558921a999c
Froot Loops <94587874+FrootLoopsBowl@users.noreply.github.com>
2022-01-06 13:56:10 +0100

    Update heyyo.sql

--- heyyo.sql
+++ heyyo.sql
@@ -1,1 +1,1 @@
-INSERT INTO `items` (`name`, `label`, `rare`, `can_remove`) VALUES ('kitneon', 'Kit Neon', '0', '1');
+INSERT INTO `items` (`name`, `label`,`limit` `rare`, `can_remove`) VALUES ('kitneon', 'Kit Neon',`-1`, '0', '1');
